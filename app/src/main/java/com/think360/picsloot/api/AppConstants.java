1fa9b0d7efd067a4ead276b820208252466d0db8
think360 <surindersingh.think360@gmail.com>
2018-03-08 11:25:05 +0530

    some chagnes in login file

--- app/src/main/java/com/think360/picsloot/api/AppConstants.java
+++
@@ -1,9 +0,0 @@
-package com.think360.picsloot.api;
-
-/**
- * Created by think360 on 18/04/17.
- */
-
-public class AppConstants {
-    public static final String BASE_URL = "http://think360.in/developer1/picsloot/api/index.php/12345/";
-}
--- app/src/main/java/com/think360/picsloot/imagepicker/PickerAction.java
+++
@@ -1,7 +0,0 @@
-package com.think360.picsloot.imagepicker;
-
-import java.util.ArrayList;
-
-public interface PickerAction {
-  void proceedResultAndFinish(ArrayList<String> selected, boolean original, int resultCode);
-}
